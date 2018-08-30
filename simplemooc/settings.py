7a72e8923a1ab52a5388bba93114728e6a840a57
geor128 <geor128@gmail.com>
2018-08-30 13:31:04 -0300

    Update settings.py
    settings ALLOWD

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j@%s#&w51+^s(d6z%dg19t!%8y-bekqtc@bio@8d#sye34)#-m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
