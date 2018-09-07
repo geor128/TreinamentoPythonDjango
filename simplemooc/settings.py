d27fa545fe9f0f74f8e55317f08ebffed03b8658
geor128 <geor128@gmail.com>
2018-09-07 14:53:47 -0300

    Adiciona STATIC_ROOT

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -120,8 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = "/var/www/djangoteste.online./static/"
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT= os.path.join(BASE_DIR, 'simplemooc', 'media')
 MEDIA_URL = '/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
