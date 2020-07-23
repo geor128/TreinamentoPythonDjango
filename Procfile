b4b2b47091f24bcae8d1d12441cad14c94e77133
George Macedo <geor128@gmail.com>
2020-07-22 23:24:24 -0300

    Painel e Editar Usuário

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn TreinamentoPythonDjango.wsgi
--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -145,3 +145,4 @@ AUTH_USER_MODEL = 'accounts.User' #para o django saber que o usuario é do nosso
   #  'static/core',
 #]
 
+django_heroku.settings(locals())
